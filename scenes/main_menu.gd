8fb8ae5697382f8765fc0a83b21042a3c1822706
catarinaflores <catarina10flores@gmail.com>
2024-01-12 16:00:34 +0000

    main menu music and buttons sound added

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -1,19 +1,25 @@
 extends Control
 
-@onready var all_scores = %AllScores
-@onready var cat_sound = %CatSound
+@onready var score = %Score
+@onready var clicked_sound = %ClickedSound
+@onready var main_menu_music = %MainMenuMusic
 
+
+func _ready():
+	main_menu_music.play()
 
 func _process(_delta):
-	all_scores.text = "High Score \n" + str(SaveSystem.data.highscore)
+	score.text = "High Score \n" + str(SaveSystem.data.highscore)
 
 
 func _on_start_button_pressed():
-	cat_sound.play()
+	clicked_sound.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://scenes/main.tscn")
 
 
 
 func _on_quit_button_pressed():
-	cat_sound.play()
+	clicked_sound.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
