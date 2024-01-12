3d62fa855b51374685213f353b32e801c7b2d5ea
catarinaflores <catarina10flores@gmail.com>
2024-01-12 17:02:42 +0000

    added sounds to game over buttons

--- scenes/game_over_menu.gd
+++ scenes/game_over_menu.gd
@@ -1,6 +1,7 @@
 extends CanvasLayer
 
 @onready var all_scores = %AllScores
+@onready var clicked_sound = %ClickedSound
 
 # Called when the node enters the scene tree for the first time.
 func _process(_delta):
@@ -9,10 +10,14 @@ func _process(_delta):
 
 
 func _on_replay_button_pressed():
+	clicked_sound.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().reload_current_scene()
 
 
 
 
 func _on_quit_button_pressed():
+	clicked_sound.play()
+	await get_tree().create_timer(0.5).timeout
 	get_tree().quit()
